7796fe01e77f0f6bf64c2d8b5e71624af2eca196
Igor Pimentel <igorpimentel@id.uff.br>
2023-11-22 13:53:55 -0300

    fix type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
 export type IProps = {
     captchaDomain: string;
     onReceiveToken: (captchaToken: string) => void;
     siteKey: string;
     action: string;
+    style?: StyleProp<ViewStyle>;
 };
 export type IState = {};
 declare class ReCaptchaV3 extends React.PureComponent<IProps, IState> {
